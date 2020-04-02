a8dd94e924b92e35a3ded5ec78931cbc9e3dc4bb
BuildTools <unconfigured@null.spigotmc.org>
2020-04-02 14:01:08 +0200

    Finished guess a number app

--- rn-guess-a-number/screens/GameScreen.js
+++ rn-guess-a-number/screens/GameScreen.js
@@ -1,5 +1,5 @@
 import React, {useState, useRef, useEffect} from 'react';
-import {View, StyleSheet, Alert} from 'react-native';
+import {View, Text, StyleSheet, Alert, ScrollView, FlatList} from 'react-native';
 import {AntDesign} from '@expo/vector-icons';
 
 import NumberContainer from '../components/NumberContainer';
@@ -18,11 +18,17 @@ const generateRandomBetween = (min, max, exclude) => {
     }
 };
 
+const renderListItem = (listLength, itemData) => (
+    <View style={styles.listItem}>
+        <BodyText>#{listLength - itemData.index}</BodyText>
+        <BodyText>{itemData.item}</BodyText>
+    </View>
+);
+
 const GameScreen = props => {
-    const [currentGuess, setCurrentGuess] = useState(
-        generateRandomBetween(1, 100, props.userChoice)
-    );
-    const [rounds, setRounds] = useState(0);
+    const initialGuess = generateRandomBetween(1, 100, props.userChoice);
+    const [currentGuess, setCurrentGuess] = useState(initialGuess); // is only set the first time because of "detached handling" where react already knows we have a set state
+    const [pastGuesses, setPastGuesses] = useState([initialGuess.toString()]);
     // useRef is values that 'survive' re-renders
     const currentLow = useRef(1);
     const currentHigh = useRef(100);
@@ -32,7 +38,7 @@ const GameScreen = props => {
     // run function *after* every render
     useEffect(() => {
         if (currentGuess === userChoice) {
-            onGameOver(rounds);
+            onGameOver(pastGuesses.length);
         }
     }, [currentGuess, userChoice, onGameOver]); // effect will only run of one if these dependencies has changed
 
@@ -44,11 +50,12 @@ const GameScreen = props => {
         if (direction === 'lower') {
             currentHigh.current = currentGuess;
         } else {
-            currentLow.current = currentGuess;
+            currentLow.current = currentGuess + 1;
         }
         const nextNumber = generateRandomBetween(currentLow.current, currentHigh.current, currentGuess);
         setCurrentGuess(nextNumber);
-        setRounds(curRounds => curRounds + 1);
+        // setRounds(curRounds => curRounds + 1);
+        setPastGuesses(curPastGuesses => [nextNumber.toString(), ...curPastGuesses]);
     };
 
     return (
@@ -63,6 +70,17 @@ const GameScreen = props => {
                     <AntDesign name="caretup" size={24} color="white"/>
                 </MainButton>
             </Card>
+            <View style={styles.listContainer}>
+            {/*<ScrollView contentContainerStyle={styles.list}>
+                {pastGuesses.map(((guess, index) => renderListItem(guess, pastGuesses.length - index)))}
+            </ScrollView>*/}
+            <FlatList
+                keyExtractor={item => item}
+                data={pastGuesses}
+                renderItem={renderListItem.bind(this, pastGuesses.length)}
+                contentContainerStyle={styles.list} // style on flatlist and scrollview allows ex. setting margin but does not allow setting position of content. For this, we use contentContainerStyle instead
+            />
+            </View>
         </View>
     );
 };
@@ -79,6 +97,24 @@ const styles = StyleSheet.create({
         marginTop: 20,
         width: 400,
         maxWidth: '90%'
+    },
+    listContainer: {
+        flex: 1,
+        width: '60%'
+    },
+    list: {
+        flexGrow: 1,
+        justifyContent: 'flex-end'
+    },
+    listItem: {
+        borderColor: '#ccc',
+        borderWidth: 1,
+        padding: 15,
+        marginVertical: 10,
+        backgroundColor: 'white',
+        flexDirection: 'row',
+        justifyContent: 'space-between',
+        width: '100%'
     }
 });
 
