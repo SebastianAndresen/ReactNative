d72412ee1d11372f8b7a50e06a909332f8f014ec
BuildTools <unconfigured@null.spigotmc.org>
2020-06-06 02:30:02 +0200

    Shop app

--- rn-shop-app/App.js
+++ rn-shop-app/App.js
@@ -1,18 +1,18 @@
 import React, {useState} from 'react';
-import {StyleSheet, Text, View} from 'react-native';
-import * as Font from 'expo-font';
-import {AppLoading} from 'expo';
-import {enableScreens} from "react-native-screens";
 import {createStore, combineReducers} from 'redux';
 import {Provider} from 'react-redux';
+import {AppLoading} from 'expo';
+import * as Font from 'expo-font';
 
-import MealsNavigator from './navigation/MealsNavigator';
-import mealsReducer from './store/reducers/meals';
-
-enableScreens();
+import productsReducer from './store/reducers/products';
+import cartReducer from './store/reducers/cart';
+import ordersReducer from './store/reducers/order';
+import ShopNavigator from './navigation/ShopNavigator';
 
 const rootReducer = combineReducers({
-    meals: mealsReducer
+    products: productsReducer,
+    cart: cartReducer,
+    orders: ordersReducer
 });
 
 const store = createStore(rootReducer);
@@ -21,19 +21,21 @@ const fetchFonts = () => {
     return Font.loadAsync({
         'open-sans': require('./assets/fonts/OpenSans-Regular.ttf'),
         'open-sans-bold': require('./assets/fonts/OpenSans-Bold.ttf')
-    })
+    });
 };
 
 export default function App() {
     const [fontLoaded, setFontLoaded] = useState(false);
 
     if (!fontLoaded) {
-        return (<AppLoading startAsync={fetchFonts} onFinish={() => setFontLoaded(true)}/>);
+        return <AppLoading startAsync={fetchFonts} onFinish={() => {
+            setFontLoaded(true);
+        }}/>
     }
 
     return (
         <Provider store={store}>
-            <MealsNavigator/>
+            <ShopNavigator/>
         </Provider>
     );
 }
--- rn-shop-app/constants/Colors.js
+++ rn-shop-app/constants/Colors.js
@@ -1,4 +1,4 @@
-export default({
-    primary: '#4a148c',
-    secondary: '#ff6f00'
-});
+export default {
+    primary: '#C2185B',
+    secondary: '#FFC107'
+}
